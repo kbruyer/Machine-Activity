4fc023a3081ec9dbcf704a28e3030cd1dac43b9f
kbruyer <38817591+kbruyer@users.noreply.github.com>
2022-07-21 12:26:41 -0700

    Update

--- machine_activity/settings.py
+++ machine_activity/settings.py
@@ -126,3 +126,5 @@ LOGOUT_URL = 'logout'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
